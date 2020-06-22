2e7e5de829f93400bfdefe77513720103a36b70b
Annour Mahamat Mahamat <doutoummahamat@gmail.com>
2020-06-22 17:45:42 +0200

    rename file

---
+++ formation2.c
@@ -0,0 +1,31 @@
+// un commentaire uniligne
+
+/**
+ * un  commentaire sur 
+ * plusieurs ligne
+*/
+#include <stdio.h>
+
+#define N 5
+// un commentaire uniligne
+
+/**
+ * un  commentaire sur 
+ * plusieurs ligne
+*/
+
+int main()
+{
+    printf("Lalekou fi giray tchad");
+    return 0;
+}
+
+/* void main()
+{
+
+    printf("Lalekou fi giray tchad 2");
+} */
+
+/* main(){
+  printf("Lalekou fi giray tchad 3");
+} */
