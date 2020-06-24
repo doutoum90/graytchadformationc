692da0a145d0b656314b5fb7413940f8b349f856
Annour Mahamat Mahamat <doutoummahamat@gmail.com>
2020-06-24 15:42:54 +0200

    save 2

--- formation9.c
+++ formation9.c
@@ -1,4 +1,36 @@
 #include <stdio.h>
+unsigned factorielwhile(unsigned n);
+unsigned factorielDowhile(unsigned n);
+unsigned factorielFor(unsigned n);
+void afficherMessage();
+
+int main()
+{
+
+    unsigned n, fact1 = 1, fact2 = 1, fact3 = 1, compteur1, compteur2, compteur3;
+    afficherMessage();
+    scanf("%u", &n);
+
+    // n!=n*(n-1)....1
+
+    // boucle while
+    // initialisation compteur
+    fact1 = factorielwhile(n);
+
+    printf("%u ! = %u \n", n, fact1);
+    // boucle do while
+    // initialisation compteur
+
+    fact2 = factorielDowhile(n);
+    printf("%u ! = %u \n", n, fact2);
+    // boucle for
+
+    fact3 = factorielFor(n);
+    printf("%u ! = %u \n", n, fact3);
+
+    return 1;
+}
+
 
 unsigned factorielwhile(unsigned n)
 {
@@ -38,30 +70,7 @@ unsigned factorielFor(unsigned n)
     return fact;
 }
 
-int main()
+void afficherMessage()
 {
-
-    unsigned n, fact1 = 1, fact2 = 1, fact3 = 1, compteur1, compteur2, compteur3;
-
     printf("veuillez saisr la valeur de n \n");
-    scanf("%u", &n);
-
-    // n!=n*(n-1)....1
-
-    // boucle while
-    // initialisation compteur
-    fact1 = factorielwhile(n);
-
-    printf("%u ! = %u \n", n, fact1);
-    // boucle do while
-    // initialisation compteur
-
-    fact2 = factorielDowhile(n);
-    printf("%u ! = %u \n", n, fact2);
-    // boucle for
-
-    fact3 = factorielFor(n);
-    printf("%u ! = %u \n", n, fact3);
-
-    return 1;
 }
